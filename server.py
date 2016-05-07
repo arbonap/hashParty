80c995b66e35c7fd1b71e4db0c3f04069196e1a5
Patricia Arbona <arbonap@gmail.com>
2016-05-07 13:50:29 -0700

    got json response back

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import os
 
 #Related Third Party Imports
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, jsonify
 
 import requests
 
@@ -22,6 +22,8 @@ def index():
 def dashboard():
 
     code = request.args.get('code')
+    print "******************************* "
+    print "code used to make access token below: *****"
     print code
 
     payload = {"client_id": os.environ['CLIENT_ID'],
@@ -32,14 +34,43 @@ def dashboard():
                }
 
     resp = requests.post("https://api.instagram.com/oauth/access_token", data=payload)
-
+    print "************************************************"
+    print "************************************************"
+    print "response object", resp
     resp_dict = resp.json()
-    print resp_dict
+    print "************************************************"
+    print "************************************************"
+    print "Response dictionary", resp_dict
     # access_token = resp_dict['access_token']
     user = resp_dict['user']['username']
-    print user
-
+    print "************************************************"
+    print "username:", user
+    token = resp_dict['access_token']
+    print "Token:", token
+    print "************************************************"
+    print "************************************************"
+    session['user_token'] = token
+    print "session:", session
+    print "************************************************"
+    print "************************************************"
     return render_template("dash.html", user=user)
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def searchresults():
+    hashtag = request.args.get('hashtag')
+    endpoint = "https://api.instagram.com/v1/tags/{}/media/recent?access_token={}".format(hashtag, session['user_token'])
+    print "************************************************"
+    print "endpoint:", endpoint
+
+    resp = requests.get(endpoint)
+    print "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    print "RESP:", resp
+    response_dict = resp.json()
+    print "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    print "Response dictionary:"
+    print response_dict
+    return jsonify(response_dict)
 
 ################################################################################
 if __name__ == "__main__":
