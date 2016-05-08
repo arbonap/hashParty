b89c4a8303c75cf90afd3e6c0ba9806ef3dc766b
Patricia Arbona <arbonap@gmail.com>
2016-05-07 18:18:25 -0700

    Filtering by created date works

--- server.py
+++ server.py
@@ -85,15 +85,61 @@ def searchresults():
     print "Start date:", startdatetime
 
     start_timestamp = time.mktime(startdatetime.timetuple())
-    print "Timestamp!!!", start_timestamp
+    print "Unix/ Epoch Timestamp!!!", start_timestamp
 
     enddatetime = datetime.datetime.strptime(enddate, "%Y-%m-%d")
     print "End date:", enddatetime
 
+    end_timestamp = time.mktime(enddatetime.timetuple())
+    filtered_results = filter_results(start_timestamp, end_timestamp, hashtag, response_dict)
     #jsonify only works on dictionaries (not on lists?)
-    return jsonify(response_dict)
+    special_thing = jsonify({'thing': filtered_results})
+    print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@: jsonify special thing @@@@@"
+    print special_thing
+    return jsonify({'thing': filtered_results})
 
 ################################################################################
+
+# The app should paginate through the endpoint and collect the content whose tag time is in
+# between the start and end dates only. The photo's tag time is defined as the time that the
+# hashtag was tagged with the photo. Normally, that is the caption created time (caption ->
+# created_time). However, if the caption does not contain the desired hashtag, but the
+# submitter of the photo posts a comment with the desired hashtag afterward, that photo will
+# be included in the pagination, under the time the comment was posted. You will want to
+# iterate through the comments key for each photo, and look for the existence of the hashtag. If
+# found, the created_time of the comment should be used as the tag time:
+
+
+def filter_results(start, end, tag, results_dict):
+    """ Filter through ugly, horrible JSON dictionary to get 'created_time'.
+        Given start and end timestamps, return only results within the start and
+        end timestamps. """
+
+    results_list = results_dict['data']
+    i = 0
+    print len(results_list)
+    while i < len(results_list):
+        text = results_list[i]['caption']['text']
+        # text_string = str(text)
+
+        # tag_string = str(tag)
+        # print "THIS IS TAG:", tag_string, "THIS IS TEXT:", text_string
+        print "I'm in the while loop here ~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        if text.find(tag) > 0: #this means it found the result. if it fails, it returns -1
+            #if tag is in the caption, go on to the next level:
+            print "I'm in the first if statment ~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            created_time = int(results_list[i]['caption']['created_time'])
+            # #if created time is outside the window of time
+            if created_time > end or created_time < start:
+                #then delete it!
+                print "second if statement~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                del results_list[i]
+        else:
+            i += 1
+    print "LENGTH OF LIST AFTER FILTERING!!!!", len(results_list)
+
+    return results_list
+
 if __name__ == "__main__":
     app.debug = True
 
